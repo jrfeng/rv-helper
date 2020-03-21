ab86c455b32ca01ddffca9806361a092c4765cfd
jrfeng <jrfeng331@qq.com>
2020-03-21 16:19:45 +0800

    更新注释

--- helper/src/main/java/recyclerview/helper/NonNullHelper.java
+++ helper/src/main/java/recyclerview/helper/NonNullHelper.java
@@ -1,5 +1,10 @@
 package recyclerview.helper;
 
+/**
+ * 用于代替 Objects.requireNonNull(T obj) 方法。
+ * <p>
+ * 因为 Objects.requireNonNull(T obj) 方法需要 API Level 19，而本项目最低需要支持到 API Level 14。
+ */
 class NonNullHelper {
     static <T> T requireNonNull(T obj) {
         if (obj == null)
