5055dbffd7aeab610349bf51785dba7668dbba96
jrfeng <jrfeng331@qq.com>
2020-03-21 16:16:45 +0800

    更新注释; 删除多余代码

--- helper/src/main/java/recyclerview/helper/SelectableAdapter.java
+++ helper/src/main/java/recyclerview/helper/SelectableAdapter.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 jrfeng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package recyclerview.helper;
 
 import androidx.annotation.NonNull;
@@ -5,6 +28,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+/**
+ * 继承该类即可让你的 RecyclerView.Adapter 具有单选和多选功能。
+ *
+ * @param <Holder> 该参数必须继承 RecyclerView.ViewHolder 类并实现 {@link SelectableHelper.Selectable}
+ *                 接口。
+ */
 public abstract class SelectableAdapter<Holder extends RecyclerView.ViewHolder & SelectableHelper.Selectable>
         extends RecyclerView.Adapter<Holder> {
     private SelectableHelper mSelectableHelper;
@@ -32,27 +61,49 @@ public abstract class SelectableAdapter<Holder extends RecyclerView.ViewHolder &
         mSelectableHelper.onBindViewHolder(holder, position);
     }
 
-    public void setSelectMode(int mode) {
+    /**
+     * 设置选择模式。
+     *
+     * @param mode 选择模式
+     */
+    public void setSelectMode(SelectableHelper.SelectMode mode) {
         mSelectableHelper.setSelectMode(mode);
     }
 
+    /**
+     * 查询某个列表项是否已被选中。
+     *
+     * @param position 要查询的列表项的位置。
+     * @return 如果 position 处的列表项已被选中，则返回 true，否则返回 false。
+     */
     public boolean isSelected(int position) {
         return mSelectableHelper.isSelected(position);
     }
 
+    /**
+     * 清除所所列表项的选中状态。
+     */
     public void clearSelected() {
         mSelectableHelper.clearSelected();
     }
 
-    public void setSelect(int position, boolean select) {
+    /**
+     * 设置某个列表项的选中状态。
+     *
+     * @param position 要选中的列表项，如果小于 0，或者超出列表项的最大索引值，则会抛出 IllegalArgumentException 异常。
+     * @param select   是否选中指定列表项。
+     * @throws IllegalArgumentException 当 position 参数小于 0，或者超出列表项的最大索引值会抛出该异常。
+     */
+    public void setSelect(int position, boolean select) throws IllegalArgumentException {
         mSelectableHelper.setSelect(position, select);
     }
 
+    /**
+     * 获取所有已被选中的列表项的索引值。
+     *
+     * @return 所有已被选中的列表项的索引值，如没有任何列表项被选中，则会返回一个空列表。
+     */
     public List<Integer> getSelectedPositions() {
         return mSelectableHelper.getSelectedPositions();
-    }
-
-    public void setOnItemClickListener(ItemClickHelper.OnItemClickListener listener) {
-        mSelectableHelper.setOnItemClickListener(listener);
     }
 }
--- helper/src/main/java/recyclerview/helper/SelectableHelper.java
+++ helper/src/main/java/recyclerview/helper/SelectableHelper.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 jrfeng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package recyclerview.helper;
 
 import androidx.annotation.NonNull;
@@ -9,7 +32,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 功能: 用于帮助属性 RecyclerView 中列表项的单选与多选功能。
+ * 用于帮助实现 RecyclerView 中列表项的单选与多选功能。
  * <p>
  * 使用方法:
  * <p>
@@ -27,22 +50,17 @@ import java.util.List;
  * <b>注意！请在 Adapter 中的数据发生改变时调用相应的 notifyXxx 方法, 或者使用 DiffUtil（<b>推荐</b>），不
  * 然 SelectableHelper 可能无法正常工作。</b>
  * <p>
- * 具体做法请参考 {@link SelectableAdapter} 的源码。
+ * 具体做法请参考 <a target="_blank" href="https://github.com/jrfeng/rv-helper/blob/master/helper/src/main/java/recyclerview/helper/SelectableAdapter.java">SelectableAdapter</a> 的源码。
  */
 public class SelectableHelper {
-    public static final int MODE_SINGLE = 0;
-    public static final int MODE_MULTIPLE = 1;
-
     @Nullable
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
 
-    private int mMode;
+    private SelectMode mSelectMode;
     private List<Integer> mSelectedPositions;
 
     private RecyclerView.AdapterDataObserver mAdapterDataObserver;
-
-    private ItemClickHelper mItemClickHelper;
 
     public SelectableHelper(@NonNull RecyclerView.Adapter adapter) {
         NonNullHelper.requireNonNull(adapter);
@@ -50,63 +68,81 @@ public class SelectableHelper {
         mAdapter = adapter;
         mRecyclerView = null;
 
-        mMode = MODE_SINGLE;
+        mSelectMode = SelectMode.SINGLE;
         mSelectedPositions = new ArrayList<>();
 
-        initListener();
-
-        mItemClickHelper = new ItemClickHelper();
+        initAdapterDataObserver();
     }
 
-
+    /**
+     * 需要在 RecyclerView.onBindViewHolder 方法中调用该方法。
+     */
     public <Holder extends RecyclerView.ViewHolder & Selectable> void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.onUnselected();
         if (mSelectedPositions.contains(position)) {
             holder.onSelected();
         }
-
-        mItemClickHelper.bindClickListener(holder.itemView);
     }
 
+    /**
+     * 附加到一个 RecyclerView 对象上。
+     *
+     * @param recyclerView 要附加到的 RecyclerView 对象，不能为 null。
+     */
     public void attachToRecyclerView(@NonNull RecyclerView recyclerView) {
         NonNullHelper.requireNonNull(recyclerView);
 
         mRecyclerView = recyclerView;
-        mItemClickHelper.attachToRecyclerView(mRecyclerView);
         registerAdapterDataObserver();
     }
 
+    /**
+     * 分离当前 {@link SelectableHelper} 对象。
+     */
     public void detach() {
         mRecyclerView = null;
-        mItemClickHelper.detach();
         unregisterAdapterDataObserver();
     }
 
+    /**
+     * 判断当前 {@link SelectableHelper} 对象是否已附加到一个 RecyclerView 对象上。
+     *
+     * @return 如果当前 {@link SelectableHelper} 对象已附加到一个 RecyclerView 对象上则返回 true，否则
+     * 返回 false。
+     */
     public boolean isAttached() {
         return mRecyclerView != null;
     }
 
-    public void setSelectMode(int mode) {
-        switch (mode) {
-            case MODE_SINGLE:
-                mMode = mode;
-                deselectOthers();
-                break;
-            case MODE_MULTIPLE:
-                mMode = mode;
-                break;
-            default:
-                break;
-        }
+    /**
+     * 设置列表的选择模式。默认为 {@link SelectMode#SINGLE} 单选模式。
+     *
+     * @param mode 选择模式。
+     */
+    public void setSelectMode(SelectMode mode) {
+        mSelectMode = mode;
 
+        if (mSelectMode == SelectMode.SINGLE) {
+            deselectOthers();
+        }
     }
 
+    /**
+     * 清除所所列表项的选中状态。
+     */
     public void clearSelected() {
         for (Integer selectedPosition : mSelectedPositions.subList(0, mSelectedPositions.size())) {
             deselect(selectedPosition);
         }
     }
 
+    /**
+     * 设置某个列表项的选中状态。
+     *
+     * @param position 要选中的列表项，如果小于 0，或者超出列表项的最大索引值，则会抛出 IllegalArgumentException 异常。
+     * @param select   是否选中指定列表项。
+     * @throws IllegalArgumentException 当 position 参数小于 0，或者超出列表项的最大索引值会抛出该异常。
+     */
     public void setSelect(int position, boolean select) throws IllegalArgumentException {
         if (position < 0) {
             throw new IllegalArgumentException("position < 0");
@@ -124,21 +160,28 @@ public class SelectableHelper {
         }
     }
 
+    /**
+     * 查询某个列表项是否已被选中。
+     *
+     * @param position 要查询的列表项的位置。
+     * @return 如果 position 处的列表项已被选中，则返回 true，否则返回 false。
+     */
     public boolean isSelected(int position) {
         return mSelectedPositions.contains(position);
     }
 
+    /**
+     * 获取所有已被选中的列表项的索引值。
+     *
+     * @return 所有已被选中的列表项的索引值，如没有任何列表项被选中，则会返回一个空列表。
+     */
     public List<Integer> getSelectedPositions() {
         return new ArrayList<>(mSelectedPositions);
     }
 
-    public void setOnItemClickListener(ItemClickHelper.OnItemClickListener listener) {
-        mItemClickHelper.setOnItemClickListener(listener);
-    }
-
     // **************************************private**************************************
 
-    private void initListener() {
+    private void initAdapterDataObserver() {
         mAdapterDataObserver = new RecyclerView.AdapterDataObserver() {
             @Override
             public void onItemRangeInserted(int positionStart, int itemCount) {
@@ -219,7 +262,7 @@ public class SelectableHelper {
             return;
         }
 
-        if (mMode == MODE_SINGLE && mSelectedPositions.size() > 0) {
+        if (mSelectMode == SelectMode.SINGLE && mSelectedPositions.size() > 0) {
             clearSelected();
         }
 
@@ -253,6 +296,20 @@ public class SelectableHelper {
             Selectable selectable = (Selectable) holder;
             selectable.onUnselected();
         }
+    }
+
+    /**
+     * 选择模式。
+     */
+    public enum SelectMode {
+        /**
+         * 单选模式。
+         */
+        SINGLE,
+        /**
+         * 多选模式。
+         */
+        MULTIPLE
     }
 
     public interface Selectable {
